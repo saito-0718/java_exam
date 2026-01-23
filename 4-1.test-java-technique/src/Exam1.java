e7396a4f63e96c1b199a1172aaee91557010f3a4
saito_masashi <masashi.saito@rakus-partners.co.jp>
2026-01-23 09:49:58 +0900

    java応用1

---
+++ 4-1.test-java-technique/src/Exam1.java
@@ -0,0 +1,10 @@
+public class Exam1 {
+
+    public static void main(String[] args) {
+        String name = "さいとうまさし";
+        int length = name.length();
+
+        System.out.println(name + "の文字数は" + length + "です");
+    }
+
+}
---
+++ 4-1.test-java-technique/src/Exam2.java
@@ -0,0 +1,9 @@
+public class Exam2 {
+
+    public static void main(String[] args) {
+        String famousSpeech = "Stay hungry, stay foolish.";
+        int startHungry = famousSpeech.indexOf("hungry");
+
+        System.out.println("hungryは" + startHungry + "番目から始まります");
+    }
+}
